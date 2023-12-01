0e38938c7bc126eaff1eb52a21e8c2f5d633357d
Ahmed Reza <mustafareza47@gmail.com>
2023-12-02 00:10:33 +0530

    nike landing page using tailwind

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./customerReviews";
+
+
+export { Hero, SpecialOffer, Subscribe, PopularProducts, Services, Footer, SuperQuality, CustomerReviews }
+
